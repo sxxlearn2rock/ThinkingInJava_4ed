36d6ba361ff2635add1ddc9d1226624ffbe9c9c3
sxx <sxx_uestc@163.com>
2015-11-05 11:29:05 +0800

    526

---
+++ src/cn/sxx/learn2java/thinkinginjava/chap21/DiningPhilosophers/Philosopher.java
@@ -0,0 +1,54 @@
+package cn.sxx.learn2java.thinkinginjava.chap21.DiningPhilosophers;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+public class Philosopher implements Runnable {
+	private Chopstick left;
+	private Chopstick right;
+	private final int id;
+	private final int ponderFactor;
+	private Random rand = new Random(47);
+	
+	public Philosopher(Chopstick left, Chopstick right,
+									int id, int ponderFactor) {
+		this.left = left;
+		this.right = right;
+		this.id = id;
+		this.ponderFactor = ponderFactor;
+	}
+	
+	private void pause() throws InterruptedException{
+		if (ponderFactor == 0) {
+			return;
+		}
+		TimeUnit.MILLISECONDS.sleep(
+				rand.nextInt(ponderFactor * 250));
+		
+	}
+
+	@Override
+	public void run() {
+		try {
+			while (! Thread.interrupted()){
+				System.out.println(this + " " + "thinking");
+				pause();
+				System.out.println(this + " " + "grabbing right");
+				right.take();
+				System.out.println(this + " " + "grabbing left");
+				left.take();
+				System.out.println(this + " " + "eating");
+				pause();
+				right.dorp();
+				left.dorp();
+			}
+		} catch (InterruptedException e) {
+			System.out.println(this + " " + "exiting via inerrupted.");
+		}
+	}
+	
+	@Override
+	public String toString() {
+		return "Philosopher #"+ id ;
+	}
+}
